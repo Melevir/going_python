9c1e90d825336600b0ecda3e1df561f6da4371b0
izomeraza <150m3raz4@gmail.com>
2019-01-25 12:45:36 +0300

    env vars, draft models

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Question, Choice, Vote
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    pass
