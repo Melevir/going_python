871a92fe17993efafe8196bf641e162555ddb8a9
izomeraza <150m3raz4@gmail.com>
2019-01-27 21:41:28 +0300

    fixes style

--- poll/admin.py
+++ poll/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import Question, Choice, Vote
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
+
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [
         ChoiceInline,
     ]
+
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
