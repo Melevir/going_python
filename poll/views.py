96b6667faff39d4ca3dac67bbec67c4ca9dd8194
izomeraza <150m3raz4@gmail.com>
2019-01-28 02:09:01 +0300

    fix check errors

--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404
 from django.views import View
 from poll.models import Question, Choice, Vote
 from django.core.exceptions import ObjectDoesNotExist
@@ -6,11 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 class ActivationView(View):
     def post(self, request, **kwargs):
-        try:
-            question = Question.objects.get(id=kwargs['question_id'])
-        except ObjectDoesNotExist:
-            return HttpResponseNotFound()
-
+        question = get_object_or_404(id=kwargs['question_id'])
         Question.objects.filter(is_active=True).update(is_active=False)
         question.is_active = True
         question.save()
@@ -37,11 +34,8 @@ class ActiveQuestionDetailView(View):
 
 class VoteView(View):
     def post(self, request, **kwargs):
-        try:
-            question = Question.objects.get(id=kwargs['question_id'])
-            option = Choice.objects.get(id=kwargs['option_id'])
-        except ObjectDoesNotExist:
-            return HttpResponseNotFound()
+        question = get_object_or_404(id=kwargs['question_id'])
+        option = get_object_or_404(id=kwargs['option_id'])
 
         if option.question != question or not question.is_active or ('user_id' not in request.GET):
             return HttpResponseBadRequest()
@@ -62,6 +56,6 @@ class StatView(View):
         hits = [{'id': choice.id, 'hits': len(choice.votes.all())} for choice in choices]
         data = {
             'stat': hits,
-            }
+        }
 
         return JsonResponse(data=data)
