96daf32a12045f3d7ab485e88e273a0fdc919a6f
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-01-31 12:44:53 +0300

    add csrf_excempt

--- poll/urls.py
+++ poll/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from poll.views import ActivationView, ActiveQuestionDetailView, VoteView, StatView, NomineesView
 
 urlpatterns = [
-    path('<int:question_id>/activate/', ActivationView.as_view()),
+    path('<int:question_id>/activate/', csrf_exempt(ActivationView.as_view())),
     path('active/', ActiveQuestionDetailView.as_view()),
     path('<int:question_id>/vote/<int:option_id>/', csrf_exempt(VoteView.as_view())),
     path('<int:question_id>/stat/', StatView.as_view()),
